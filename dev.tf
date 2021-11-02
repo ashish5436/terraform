9bb050851ac47a753f7bda856c70fc7f96df339a
ashish5436 <89197144+ashish5436@users.noreply.github.com>
2021-11-03 01:43:41 +0530

    Update dev.tf

--- dev.tf
+++ dev.tf
@@ -1,136 +1,207 @@
 provider "aws" {
-  region     = "ap-south-1"
-  access_key = "AKIAZS6HQ2URKA3C4U7R"
-  secret_key = "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"
+  access_key = "AKIAZS6HQ2URP7J7UBFO"
+  secret_key = "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"
+  region     = "us-east-1"
 }
-# # 1. Create vpc
-resource "aws_vpc" "dev-vpc" {
-   cidr_block = "10.0.0.0/16"
+
+resource "aws_vpc" "tf_vpc" {
+  cidr_block = "172.0.0.0/16"
+  enable_dns_hostnames = true
    tags = {
      Name = "development"
    }
  }
 
-# # 2. Create Internet Gateway
+
+resource "aws_subnet" "tf_public" {
+  vpc_id = "${aws_vpc.tf_vpc.id}"
+  cidr_block = "172.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Web_Public_Subnet"
+  }
+}
+
+
+resource "aws_subnet" "tf_private" {
+  vpc_id = "${aws_vpc.tf_vpc.id}"
+  cidr_block = "172.0.2.0/24"
+availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "Db_Private_Subnet"
+  }
+}
+
+
 resource "aws_internet_gateway" "gw" {
-   vpc_id = aws_vpc.dev-vpc.id
-}
-# # 3. Create Custom Route Table
-resource "aws_route_table" "dev-route-table" {
-   vpc_id = aws_vpc.dev-vpc.id
+  vpc_id = "${aws_vpc.tf_vpc.id}"
 
-   route {
-     cidr_block = "0.0.0.0/0"
-     gateway_id = aws_internet_gateway.gw.id
-   }
-
-   route {
-     ipv6_cidr_block = "::/0"
-     gateway_id      = aws_internet_gateway.gw.id
-   }
-
-   tags = {
-     Name = "Prod"
-   }
+  tags = {
+    Name = "tf_VPC_IGW"
+  }
 }
 
-# # 4. Create a Subnet 
-resource "aws_subnet" "subnet-1" {
-   vpc_id            = aws_vpc.dev-vpc.id
-   cidr_block        = "10.0.1.0/24"
-   availability_zone = "ap-south-1a"
 
-   tags = {
-     Name = "prod-subnet"
-   }
+
+# Define the route table
+resource "aws_route_table" "web-public-rt" {
+  vpc_id = "${aws_vpc.tf_vpc.id}"
+
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+
+  tags = {
+    Name = "Public Subnet RT"
+  }
 }
 
-# # 5. Associate subnet with Route Table
-resource "aws_route_table_association" "a" {
-   subnet_id      = aws_subnet.subnet-1.id
-   route_table_id = aws_route_table.dev-route-table.id
-}
-# # 6. Create Security Group to allow port 22,80,443
-resource "aws_security_group" "allow_web" {
-   name        = "allow_web_traffic"
-   description = "Allow Web inbound traffic"
-   vpc_id      = aws_vpc.dev-vpc.id
-
-   ingress {
-     description = "HTTPS"
-     from_port   = 443
-     to_port     = 443
-     protocol    = "tcp"
-     cidr_blocks = ["0.0.0.0/0"]
-   }
-   ingress {
-     description = "HTTP"
-     from_port   = 80
-     to_port     = 80
-     protocol    = "tcp"
-     cidr_blocks = ["0.0.0.0/0"]
-   }
-   ingress {
-     description = "SSH"
-     from_port   = 22
-     to_port     = 22
-     protocol    = "tcp"
-     cidr_blocks = ["0.0.0.0/0"]
-   }
-
-   egress {
-     from_port   = 0
-     to_port     = 0
-     protocol    = "-1"
-     cidr_blocks = ["0.0.0.0/0"]
-   }
-
-   tags = {
-     Name = "allow_web"
-   }
+# Assign the route table to the public Subnet
+resource "aws_route_table_association" "web-public-rt" {
+  subnet_id = "${aws_subnet.tf_public.id}"
+  route_table_id = "${aws_route_table.web-public-rt.id}"
 }
 
-# # 7. Create a network interface with an ip in the subnet that was created in step 4
-resource "aws_network_interface" "web-server-nic" {
-   subnet_id       = aws_subnet.subnet-1.id
-   private_ips     = ["10.0.1.50"]
-   security_groups = [aws_security_group.allow_web.id]
+
+# Define the security group for public subnet
+resource "aws_security_group" "sglb" {
+  name = "vpc_test_lb"
+  description = "Allow incoming HTTP connections to LB "
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  vpc_id="${aws_vpc.tf_vpc.id}"
+
+  tags = {
+    Name = "lb_SG"
+  }
 }
 
-# # 8. Assign an elastic IP to the network interface created in step 7
+# Define the security group for web server
 
-resource "aws_eip" "one" {
-   vpc                       = true
-   network_interface         = aws_network_interface.web-server-nic.id
-   associate_with_private_ip = "10.0.1.50"
-   depends_on                = [aws_internet_gateway.gw]
+# Define the security group for public subnet
+
+resource "aws_security_group" "sgweb" {
+  name = "vpc_test_web"
+  description = "Allow incoming HTTP connections & SSH access "
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    security_groups =["${aws_security_group.sglb.id}"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    security_groups =["${aws_security_group.sglb.id}"]
+  }
+
+
+
+  ingress {
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks =  ["0.0.0.0/0"]
+  }
+
+ egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  vpc_id="${aws_vpc.tf_vpc.id}"
+
+  tags = {
+    Name = "lb_SG"
+  }
 }
 
- output "server_public_ip" {
-   value = aws_eip.one.public_ip
- }
 
-# # 9. Create Ubuntu server and install/enable apache2
+# Define the security group for private subnet
+resource "aws_security_group" "sgdb"{
+  name = "sg_test_web"
+  description = "Allow traffic from public subnet"
 
-resource "aws_instance" "Dev-server-instance" {
-   ami               = "ami-041db4a969fe3eb68"
-   instance_type     = "t2.micro"
-   availability_zone = "ap-south-1a"
-   key_name          = "Devops"
+  ingress {
+    from_port = 3306
+    to_port = 3306
+    protocol = "tcp"
+    security_groups =["${aws_security_group.sgweb.id}"]
+  }
 
-   network_interface {
-     device_index         = 0
-     network_interface_id = aws_network_interface.web-server-nic.id
-   }
+  ingress {
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+    cidr_blocks = ["172.0.1.0/24"]
+  }
 
-#   user_data = <<-EOF
-#                 #!/bin/bash
-#                 sudo apt update -y
-#                 sudo apt install apache2 -y
-#                 sudo systemctl start apache2
-#                 sudo bash -c 'echo your very first web server > /var/www/html/index.html'
-#                 EOF
-#   tags = {
-#     Name = "web-server"
-#   }
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["172.0.1.0/24"]
+  }
+
+  vpc_id = "${aws_vpc.tf_vpc.id}"
+
+  tags = {
+    Name = "DB SG"
+  }
+}
+
+# Define webserver inside the public subnet
+resource "aws_instance" "wb" {
+   ami  = "ami-06b5810be11add0e2"
+   instance_type = "t1.micro"
+   key_name = "Devops"
+   subnet_id = "${aws_subnet.tf_public.id}"
+   vpc_security_group_ids = ["${aws_security_group.sgweb.id}"]
+   associate_public_ip_address = true
+   user_data = <<-EOF
+                #!/bin/bash
+                sudo apt-get update
+                sudo apt-get install apache2 -y
+                cd /home/ubuntu/
+                EOF
+
+  tags = {
+    Name = "webserver_14.04"
+  }
 }
